b1fccd7447f2285518c57357efaa43467152e310
Max <maxh2709@gmail.com>
2021-06-20 17:34:31 +0200

    Add Mojang's hardcoded checks to hammers as well

--- src/main/java/dev/ftb/mods/sluice/item/HammerItem.java
+++ src/main/java/dev/ftb/mods/sluice/item/HammerItem.java
@@ -4,7 +4,9 @@ import dev.ftb.mods.sluice.FTBSluice;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.ToolType;
 
 import java.util.HashSet;
@@ -20,6 +22,24 @@ public class HammerItem extends DiggerItem {
         super(attackBase, attackSpeed, tier, new HashSet<>(), !fireResistant
                 ? props.addToolType(ToolType.PICKAXE, tier.getLevel()).addToolType(ToolType.SHOVEL, tier.getLevel()).durability(tier.getUses())
                 : props.addToolType(ToolType.PICKAXE, tier.getLevel()).addToolType(ToolType.SHOVEL, tier.getLevel()).durability(tier.getUses()).fireResistant());
+    }
+
+    @Override
+    public boolean isCorrectToolForDrops(BlockState state) {
+        ToolType harvestTool = state.getHarvestTool();
+        if (harvestTool == ToolType.PICKAXE || harvestTool == ToolType.SHOVEL) {
+            if (this.getTier().getLevel() >= state.getHarvestLevel()) {
+                return true;
+            }
+        }
+        // Mojank hardcoded shovel checks
+        if (state.is(Blocks.SNOW) || state.is(Blocks.SNOW_BLOCK)) {
+            return true;
+        }
+        // Mojank hardcoded pickaxe checks
+        // ...god am I glad this is going away in 1.17
+        Material material = state.getMaterial();
+        return material == Material.STONE || material == Material.METAL || material == Material.HEAVY_METAL;
     }
 
     @Override
